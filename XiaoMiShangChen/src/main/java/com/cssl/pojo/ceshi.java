b983e87a43384a835fb7e08b61069d8f56d0ec23
songguanglong <2571044305@qq.com>
2019-09-21 17:34:55 +0800

    测试ssss

--- XiaoMiShangChen/src/main/java/com/cssl/pojo/ceshi.java
+++ XiaoMiShangChen/src/main/java/com/cssl/pojo/ceshi.java
@@ -4,6 +4,6 @@ public class ceshi {
     //一个测试1
     public static void main(String[] args) {
         //写了个main方法
-       System.out.println("dsfsdfsadfasdkfsjdsafkds");
+       System.out.println("13213121321iiiii");
     }
 }
